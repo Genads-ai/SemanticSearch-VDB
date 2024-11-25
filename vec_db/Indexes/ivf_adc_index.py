a22f98d4c2c729abd0c04b1347b4e9ad94fdd244
OmarMah <36341168+OmarMah@users.noreply.github.com>
2024-11-25 02:33:54 +0200

    Added the IVF part only to the ivf_adc_index

--- vec_db/Indexes/ivf_adc_index.py
+++ vec_db/Indexes/ivf_adc_index.py
@@ -1,38 +1,67 @@
 import numpy as np
 import os
+import sys
 from .indexing_strategy import IndexingStrategy
+import faiss
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utilities import compute_recall_at_k
 
 class IVFADCIndex(IndexingStrategy):
-    def __init__(self):
-        self.index = None 
+    def __init__(self,index = None) -> None:
+        self.index = index
 
-    def build_index(self, vectors: np.ndarray):
-        print("Building the IVFADC index")
-        
-        # Create a folder called DB_Indexes to save the index , this folder should be in the root of the project
-        os.makedirs("DBIndexes", exist_ok=True)
+    def build_index(self, vectors: np.ndarray):        
+        # Get the size of the vectors
+        nq, d = vectors.shape
+        # Construct the IVF Index
+        index_f = faiss.index_factory(d, "IVF4,Flat")
+        index_f.train(vectors)
+        index_f.add(vectors)
+
+        # # Construct the ground truth index
+        # exact_index = faiss.IndexFlatL2(d)
+        # exact_index.add(vectors)
+
+        # # Generate some random query vector
+        # xq = np.random.random((100, d)).astype(np.float32)
+
+        # # Define the number of nearest neighbors to search for
+        # k = 5
+
+        # D, I = index_f.search(xq, k)
+
 
         
-        # Placeholder for actual IVFADC index building logic
-        self.index = self._generate_ivfadc_index(vectors)
-        print("IVFADC index built successfully.")
+        # # Perform an exact search to get ground truth
+
+        # D_exact, I_exact = exact_index.search(xq, k)
+        # print(f"Nearest neighbors (Ground Truth): {I_exact}")
+        # print(f"Distances (Ground Truth): {D_exact}")
+        # print(f"Nearest neighbors: {I}")
+        # print(f"Distances: {D}")
+
+        # # Evaluate recall
+        # recall = compute_recall_at_k(I_exact, I, k)
+        # print(f"Recall@{k}: {recall:.4f}")
+
+
+
+        self.index = index_f
+        # Name the file in this format indextype_db_size
+        # Create the foler DBIndexes in the root directory
+        self.save_index(f"DBIndexes/ivf_adc_index_{nq}")
+        return index_f
 
     def save_index(self, path: str):
-        if self.index is None:
-            raise ValueError("Index not built. Build the index before saving.")
-        
-        print("Saving the IVFADC index")
-        
-        # Create a folder called DB_Indexes to save the index , this folder should be in the root of the project
-        os.makedirs("DBIndexes", exist_ok=True)
-        
-        # Placeholder for saving logic
-        np.save(path, self.index)
-        print(f"IVFADC index saved at: {path}")
+        print(f"Saving index to {path}")
+        faiss.write_index(self.index, path)
 
-    def _generate_ivfadc_index(self, vectors: np.ndarray):
-        # Placeholder for actual IVFADC index building logic
-        return np.random.random((10, 70))  
+    def load_index(self, path: str):
+        self.index = faiss.read_index(path)
+        return self.index
+    
     
 
 
